c200ef0740b9303a07ca22214c260abfc70db2f7
v_stoilo <130467504+MrKatafan100@users.noreply.github.com>
2024-04-13 15:39:23 +0300

    Update remvsk.py

--- remvsk.py
+++ remvsk.py
@@ -29,7 +29,7 @@ async def on_member_join(member):
 		icon_url = "https://cdn.discordapp.com/attachments/1207240200758108181/1228634187385278525/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU.png?ex=662cc1c1&is=661a4cc1&hm=855c4eb3755fa480f4dd2dac41d40d77d632b421cdd83a884d9f1eab185c1987&"
 	)
 	
-	embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1211575984483078206/1228591262450585631/3.png?ex=662c99c7&is=661a24c7&hm=d4b9580d8a9637fb3e46b6567398d75a57d8eeff14c43e2f51671841c4d10e33&")
+	embed.set_thumbnail(url="")
 
 	await member.add_roles(role, role2)
 	await chanel.send(embed=embed)
