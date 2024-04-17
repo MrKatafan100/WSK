a4ca9e81378f45c44a3466593c6593fe12fc3689
v_stoilo <130467504+MrKatafan100@users.noreply.github.com>
2024-04-17 20:02:30 +0300

    Update remvsk.py

--- remvsk.py
+++ remvsk.py
@@ -28,22 +28,49 @@ async def on_member_join(member):
 	role = disnake.utils.get(guild.roles, name="Гражданский")
 	role2 = disnake.utils.get(guild.roles, name="Налог")
 	chanel = disnake.utils.get(guild.channels, name="👨👩гражданины")
-	embed = Embed(
+	server_id = member.guild.id
+
+	cursor.execute("SELECT * FROM local WHERE server = ?", (server_id,))
+	result = cursor.fetchone()
+
+	if result:
+		language = result[1]
+	else:
+		await print("Не выбран язык")
+
+	embedr = Embed(
 		title="Этого мобилизируем↓↓↓",
 		description=f"Новый Участник! {ping} тобі пізда.",
 		colour = 000000
 	)
 	
-	embed.set_author(
+	embedr.set_author(
+		name = "by: v_stoilo",
+		url = "https://github.com/MrKatafan100",
+		icon_url = "https://cdn.discordapp.com/attachments/1207240200758108181/1228634187385278525/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU.png?ex=662cc1c1&is=661a4cc1&hm=855c4eb3755fa480f4dd2dac41d40d77d632b421cdd83a884d9f1eab185c1987&"
+	)
+
+	embeda = Embed(
+		title="Mobilizing this one↓↓↓",
+		description=f"New Member! {ping}, you're screwed.",
+		colour = 000000
+	)
+	
+	embeda.set_author(
 		name = "by: v_stoilo",
 		url = "https://github.com/MrKatafan100",
 		icon_url = "https://cdn.discordapp.com/attachments/1207240200758108181/1228634187385278525/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU.png?ex=662cc1c1&is=661a4cc1&hm=855c4eb3755fa480f4dd2dac41d40d77d632b421cdd83a884d9f1eab185c1987&"
 	)
 	
-	embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1211575984483078206/1228591262450585631/3.png?ex=662c99c7&is=661a24c7&hm=d4b9580d8a9637fb3e46b6567398d75a57d8eeff14c43e2f51671841c4d10e33&")
-
-	await member.add_roles(role, role2)
-	await chanel.send(embed=embed)
+	embedr.set_thumbnail(url="https://cdn.discordapp.com/attachments/1211575984483078206/1228591262450585631/3.png?ex=662c99c7&is=661a24c7&hm=d4b9580d8a9637fb3e46b6567398d75a57d8eeff14c43e2f51671841c4d10e33&")
+	embeda.set_thumbnail(url="https://cdn.discordapp.com/attachments/1211575984483078206/1228591262450585631/3.png?ex=662c99c7&is=661a24c7&hm=d4b9580d8a9637fb3e46b6567398d75a57d8eeff14c43e2f51671841c4d10e33&")
+	
+	if language == False:
+		await member.add_roles(role, role2)
+		await chanel.send(embed=embedr)
+	else:
+		await member.add_roles(role, role2)
+		await chanel.send(embed=embeda)		
 
 @has_permissions(ban_members=True)
 @bot.slash_command(
@@ -150,6 +177,46 @@ async def mute(ctx, user: disnake.Member, time: int, reason: str):
 			await ctx.send(f"User {ping} has been muted due to {reason}.")
 		else:
 			await ctx.send("Did you want to mute an admin?")
+
+# только для быдловска
+@has_permissions(administrator=True)
+@bot.slash_command(
+	name="bidlovsk_only_arest",
+	description="арестовует участника",
+	options=[
+	disnake.Option("user","пользыватель", type=disnake.OptionType.user, required=True)
+	]
+)
+async def arest(ctx, user: disnake.Member):
+	ping = user.mention
+	roles = user.roles
+	server_id = ctx.guild.id
+	guild = ctx.guild
+	role = disnake.utils.get(guild.roles, name="Арестованный")
+
+	cursor.execute("SELECT * FROM local WHERE server = ?", (server_id,))
+	result = cursor.fetchone()
+
+	if result:
+		language = result[1]
+	else:
+		language = None	
+
+	if language == False:
+		if ctx.author.top_role.position > user.top_role.position:
+			await user.edit(roles=[])
+			await user.add_roles(role)
+			await ctx.send(f"Пользыватель {ping} был арестован.")
+		else:
+			await ctx.send("Ты хотел арестовать админа? ІДІ нахуй.")
+	else:
+		if ctx.author.top_role.position > user.top_role.position:
+			await user.edit(roles=[])
+			await user.add_roles(role)
+			await ctx.send(f"User {ping} has been arrested.")
+		else:
+			await ctx.send("You wanted to arrest an admin?")
+# только для быдловска
 
 @has_permissions(administrator=True)
 @bot.slash_command(
